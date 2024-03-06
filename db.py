66290f30347aafead127894a43e53a114648aa84
iltenahmet <iltenahmet@gmail.com>
2024-03-06 17:23:51 -0500

    config for deployment

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ db_name = os.getenv("MONGO_DBNAME")
 # Display error message if mongoDB environment is not set up
 if not uri or not db_name:
     error_message = "MongoDB environment is not set up. Please check your environment variables."
-    app.logger.error(error_message)
+    #app.logger.error(error_message)
     raise EnvironmentError(error_message)
 
 # Create a new client and connect to the server
